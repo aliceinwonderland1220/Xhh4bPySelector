d5a153b9121fb41d30789db33165837bbc40a277
makagan <makagan@slac.stanford.edu>
2015-11-03 07:14:04 -0800

    updated checkout script

--- External/checkout.sh
+++ External/checkout.sh
@@ -1,13 +1,31 @@
+#!/bin/bash
+
+user=qzeng
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -u | --user )           shift
+                                user=$1
+                                ;;
+        * )                     echo "usage: source checkout.sh [[-u | --user] USERNAME]"
+                                exit 1
+    esac
+    shift
+done
+
+
+
+
 # AutoHists
 mkdir -p AutoHists
 cd AutoHists
-svn co svn+ssh://qzeng@svn.cern.ch/reps/atlasinst/Institutes/SLAC/AutoHists/trunk AutoHists
+svn co svn+ssh://$user@svn.cern.ch/reps/atlasinst/Institutes/SLAC/AutoHists/trunk AutoHists
 ln -s AutoHists/cmt/domake.sh
 ln -s AutoHists/cmt/doclean.sh
 cd ..
 
 # ProofAnaCore
-svn co svn+ssh://qzeng@svn.cern.ch/reps/atlasinst/Institutes/SLAC/ProofAnaCore/trunk ProofAnaCore
+svn co svn+ssh://$user@svn.cern.ch/reps/atlasinst/Institutes/SLAC/ProofAnaCore/trunk ProofAnaCore
 
 # PileupReweighting
-svn co svn+ssh://qzeng@svn.cern.ch/reps/atlasoff/PhysicsAnalysis/AnalysisCommon/PileupReweighting/tags/PileupReweighting-00-03-11 PileupReweighting
+svn co svn+ssh://$user@svn.cern.ch/reps/atlasoff/PhysicsAnalysis/AnalysisCommon/PileupReweighting/tags/PileupReweighting-00-03-11 PileupReweighting
